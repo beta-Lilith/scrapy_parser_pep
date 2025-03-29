ef721fd0e81baa7ee01bd58f3d1d163a871ea0da
Kseniia_O <liprekqn@gmail.com>
2025-03-29 18:44:31 +0300

    corrections for tests 3

--- pep_parse/settings.py
+++ pep_parse/settings.py
@@ -1,4 +1,3 @@
-# flake8: noqa
 # Scrapy settings for pep_parse project
 #
 # For simplicity, this file contains only settings considered important or
@@ -39,7 +38,8 @@ ROBOTSTXT_OBEY = True
 
 # Override the default request headers:
 # DEFAULT_REQUEST_HEADERS = {
-#   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+#   'Accept':
+# 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
 #   'Accept-Language': 'en',
 # }
 
@@ -81,7 +81,8 @@ ITEM_PIPELINES = {
 # AUTOTHROTTLE_DEBUG = False
 
 # Enable and configure HTTP caching (disabled by default)
-# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
+# See https://docs.scrapy.org/en/latest/topics/
+# downloader-middleware.html#httpcache-middleware-settings
 # HTTPCACHE_ENABLED = True
 # HTTPCACHE_EXPIRATION_SECS = 0
 # HTTPCACHE_DIR = 'httpcache'
