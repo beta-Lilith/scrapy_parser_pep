c4f77547bcb4ef70c12ea51bf8239d27ce0b4ac2
Kseniia_O <liprekqn@gmail.com>
2025-03-29 18:36:20 +0300

    corrections for tests

--- pep_parse/settings.py
+++ pep_parse/settings.py
@@ -63,7 +63,7 @@ ROBOTSTXT_OBEY = True
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-   'pep_parse.pipelines.PepParsePipeline': 300,
+    'pep_parse.pipelines.PepParsePipeline': 300,
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
